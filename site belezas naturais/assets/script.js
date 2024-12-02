bee1ca5073c29e7f8871989330728a6c4c8a089b
Williamelo <91398819+Williamell2@users.noreply.github.com>
2024-12-01 22:01:55 -0400

    Add files via upload
    atualização

---
+++ site belezas naturais/assets/script.js
@@ -0,0 +1,18 @@
+
+        function mostrarOcultar1() {
+            var texto = document.getElementById("texto1");
+            if (texto.style.display === "none") {
+                texto.style.display = "block";
+            } else {
+                texto.style.display = "none";
+            }
+        }
+        function mostrarOcultar2() {
+            var texto = document.getElementById("texto2");
+            if (texto.style.display === "none") {
+                texto.style.display = "block";
+            } else {
+                texto.style.display = "none";
+            }
+        }
+    
